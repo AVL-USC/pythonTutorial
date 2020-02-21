96e48d2d3169c59153a49803bdde79d97f9940d4
asvilesov <asvilesov@gmail.com>
2020-02-20 18:32:03 -0800

    a

--- test_git.py
+++ test_git.py
@@ -1,11 +1,8 @@
 
-<<<<<<< HEAD
 def adding(a, b):
     return a+b
-=======
 def subtract(a,b):
     return a-b
->>>>>>> new_feature
 
 if __name__=="__main__":
     print("hi this works")
@@ -13,8 +10,5 @@ if __name__=="__main__":
     x = 5
     y = 3
 
-<<<<<<< HEAD
     print(adding(x,y))
-=======
     print(subtract(x,y))
->>>>>>> new_feature
