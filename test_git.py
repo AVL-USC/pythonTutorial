c6a5491c985d64983536648d3a0ccc1742e4e259
asvilesov <asvilesov@gmail.com>
2020-02-20 18:27:23 -0800

    l

--- test_git.py
+++ test_git.py
@@ -6,9 +6,6 @@ if __name__=="__main__":
     print("hi this works")
     print("I added this extra code")
     x = 5
-<<<<<<< HEAD
     y = 3
 
     print(adding(x,y))
-=======
->>>>>>> f079a60a04e5082f101165e08241017bc8d2893f
